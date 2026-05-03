48d21fecd038006223b345583e268bebe3d36826
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-05-03 20:53:50 -0300

    fix: corrigindo erro na pipeline de CI

--- src/test/java/com/Codexsystem/Basilico/Basilico/LobarApplicationTests.java
+++ src/test/java/com/Codexsystem/Basilico/Basilico/LobarApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LobarApplicationTests {
+class BasilicoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
