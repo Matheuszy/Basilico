b68dd5e8ba5bbf8498b713d9d7c1b6d38fb38619
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-05-03 15:26:46 -0300

    feat: adicionando pedidoservice

--- src/main/java/com/Codexsystem/Basilico/Basilico/cardapio/services/PedidoService.java
+++ src/main/java/com/Codexsystem/Basilico/Basilico/cardapio/services/PedidoService.java
@@ -1,4 +1,63 @@
 package com.Codexsystem.Basilico.Basilico.cardapio.services;
 
+import com.Codexsystem.Basilico.Basilico.cardapio.model.Bebida;
+import com.Codexsystem.Basilico.Basilico.cardapio.model.Pedido;
+import com.Codexsystem.Basilico.Basilico.cardapio.model.Refeicao;
+import com.Codexsystem.Basilico.Basilico.cardapio.repository.PedidoRepository;
+import com.Codexsystem.Basilico.Basilico.usuario.model.Cliente;
+import com.Codexsystem.Basilico.Basilico.usuario.repository.ClienteRepository;
+import com.Codexsystem.Basilico.Basilico.usuario.services.ClienteService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Transactional
+    public Pedido criarPedido(Integer clienteId, List<Bebida> bebida, List<Refeicao> refeicao) {
+        Cliente cliente = clienteRepository.findById(clienteId)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        Pedido pedido = new Pedido();
+        pedido.setRefeicoes(refeicao);
+        pedido.setBebidas(bebida);
+        pedido.calcularValorTotal();
+
+        cliente.criarPedido(pedido);
+
+        return pedidoRepository.save(pedido);
+    }
+
+    @Transactional
+    public Pedido updateRefeicao(Long pedidoId, List<Refeicao> refeicao) {
+        Pedido pedido = pedidoRepository.findById(pedidoId)
+                .orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+
+        pedido.setRefeicoes(refeicao);
+        pedido.calcularValorTotal();
+
+        return pedidoRepository.save(pedido);
+    }
+
+    @Transactional
+    public Pedido updateBebida(Long pedidoId, List<Bebida> bebida) {
+        Pedido pedido = pedidoRepository.findById(pedidoId)
+                .orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+
+        pedido.setBebidas(bebida);
+        pedido.calcularValorTotal();
+
+        return pedidoRepository.save(pedido);
+    }
+
+
 }
