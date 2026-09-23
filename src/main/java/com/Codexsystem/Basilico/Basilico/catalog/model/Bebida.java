d488925da2ae1cb66b7554fa9d4fd33b9ea04791
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-09-22 22:39:51 -0300

    refactor: remove dependencia do lombok aos modelos bebida e refeicao

--- src/main/java/com/Codexsystem/Basilico/Basilico/catalog/model/Bebida.java
+++ src/main/java/com/Codexsystem/Basilico/Basilico/catalog/model/Bebida.java
@@ -1,14 +1,10 @@
 package com.Codexsystem.Basilico.Basilico.catalog.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
-@Getter
-@Setter
 @Table(name = "bebidas")
 public class Bebida {
 
@@ -29,6 +25,38 @@ public class Bebida {
     public Bebida(String nome, String descricao, BigDecimal valor) {
         this.nome = nome;
         this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
         this.valor = valor;
     }
 }
