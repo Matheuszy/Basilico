217533fb74c22da553ad66fa5a1d32960786fd7c
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-09-22 22:31:31 -0300

    refactor: Refatora service de bebida e implementa retorno http

---
+++ src/test/java/com/Codexsystem/Basilico/Basilico/catalog/services/BebidaServiceTest.java
@@ -0,0 +1,10 @@
+package com.Codexsystem.Basilico.Basilico.catalog.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith((MockitoExtension.class))
+public class BebidaServiceTest {
+
+
+}
